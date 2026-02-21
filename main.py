dc4b3dfb7a0121c6a9369fabd92b2a51c7f842fd
John Arban Lewis <johnarban@users.noreply.github.com>
2026-02-21 14:20:26 -0500

    use uv for python stuff

---
+++ main.py
@@ -0,0 +1,12 @@
+def main():
+    print("Hello from astronomy-libraries-test!")
+    import astropy
+    print("Successfully import astropy")
+    import matplotlib
+    print("Successfully import matplotlib")
+    import skyfield
+    print("Successfully import skyfield")
+
+
+if __name__ == "__main__":
+    main()
